4119a61588f46be3e209b758b2ffd76d9eaa53ef
manish-partey <manishk.partey@gmail.com>
2023-07-11 12:39:32 +0000

    Successful captcha message

--- AaravEnterprise/Controllers/SendEmailController.cs
+++ AaravEnterprise/Controllers/SendEmailController.cs
@@ -42,7 +42,7 @@ namespace AaravEnterprise.Controllers
                 MessageBody.Append("<br />");
                 MessageBody.Append("Aarav Enterprise");
                 _emailSender.SendEmail(email, "Automatic Reply - Please do not reply", MessageBody.ToString());
-                TempData["success"] = "Thank you, We you get back to you shortly !";
+                TempData["ValidCaptcha"] = "Thank you for your email, We will get back to you shortly !";
                 return RedirectToAction("Contact", "Home");
             }
             else
@@ -72,7 +72,7 @@ namespace AaravEnterprise.Controllers
                 MessageBody.Append("<br />");
                 MessageBody.Append("Aarav Enterprise");
                 _emailSender.SendEmail(email, "Automatic Reply - Please do not reply", MessageBody.ToString());
-                TempData["success"] = "Thank you, We you get back to you shortly !";
+                TempData["ValidCaptcha"] = "Thank you for your email, We will get back to you shortly !";
                 return RedirectToAction("Contact", "Home");
             }
             else
